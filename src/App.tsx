6329717e407720b1cbe361af5135b37b74f7f532
LFSoto <soto2197@gmail.com>
2022-06-13 21:51:34 -0600

    Adding the basic implementation of the first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component, ReactNode } from 'react';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dummyProducts = [
+  { name: 'Tomato', price: 1500, img: '/products/tomate.jpg' }, 
+  { name: 'Arbejas', price: 2500, img: '/products/arbejas.jpg' },
+  { name: 'Lechuga', price: 500, img: '/products/lechuga.jpg' },
+]
+class App extends Component{
+  products:any = dummyProducts;
+ 
+  render(): ReactNode {
+    return (
+      <div>
+        <Products products={this.products}/>
+      </div>
+    );
+  }
 }
 
 export default App;
