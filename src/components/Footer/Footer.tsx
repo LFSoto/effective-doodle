a205dea1df0a75d3d07da59ffa016d937ef50a60
LFSoto <soto2197@gmail.com>
2022-06-23 20:10:13 -0600

    Adding footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+const styles = {
+    footer: {
+        backgroundColor: '#0A283E', 
+        color: '#fff', 
+        height: '100vh',
+        marginTop: '100%',
+        position:'sticky' as 'sticky'
+    }
+}
+
+const Footer = () => {
+  return <div style={styles.footer}>Footer</div>;
+};
+
+export default Footer;
